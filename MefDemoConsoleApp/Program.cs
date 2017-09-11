ce561491015a56824d58dcb4a54cdc5368030f1f
cailin0630 <903199067@qq.com>
2017-09-11 09:59:47 +0800

    import

---
+++ MefDemoConsoleApp/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace MefDemoConsoleApp
+{
+    internal class Program
+    {
+        [Import(typeof(ICalculator))]
+        public ICalculator Calculator { get; set; }
+
+        private CompositionContainer _container;
+
+        private Program()
+        {
+            var catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
+            _container = new CompositionContainer(catalog);
+            try
+            {
+                _container.ComposeParts(this);
+            }
+            catch (CompositionException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Program p = new Program(); //Composition is performed in the constructor
+            String s;
+            Console.WriteLine("Enter Command:");
+            while (true)
+            {
+                s = Console.ReadLine();
+                Console.WriteLine(p.Calculator.Calculate(s));
+            }
+        }
+    }
+
+    public interface ICalculator
+    {
+        String Calculate(String input);
+    }
+
+    [Export(typeof(ICalculator))]
+    public class MyCalculator : ICalculator
+    {
+        [ImportMany]
+        private IEnumerable<Lazy<IOperation, IOperationData>> operations;
+
+      
+
+        [Export(typeof(IOperation))]
+        [ExportMetadata("Symbol", '+')]
+        class Add : IOperation
+        {
+            public int Operate(int left, int right)
+            {
+                return left + right;
+            }
+        }
+        public String Calculate(String input)
+        {
+            int left;
+            int right;
+            Char operation;
+            int fn = FindFirstNonDigit(input); //finds the operator  
+            if (fn < 0) return "Could not parse command.";
+
+            try
+            {
+                //separate out the operands  
+                left = int.Parse(input.Substring(0, fn));
+                right = int.Parse(input.Substring(fn + 1));
+            }
+            catch
+            {
+                return "Could not parse command.";
+            }
+
+            operation = input[fn];
+
+            foreach (Lazy<IOperation, IOperationData> i in operations)
+            {
+                if (i.Metadata.Symbol.Equals(operation))
+                    return i.Value.Operate(left, right).ToString();
+            }
+            return "Operation Not Found!";
+        }
+        private int FindFirstNonDigit(String s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!(Char.IsDigit(s[i]))) return i;
+            }
+            return -1;
+        }
+
+        [Export(typeof(IOperation))]
+        [ExportMetadata("Symbol", '-')]
+        class Subtract : IOperation
+        {
+            public int Operate(int left, int right)
+            {
+                return left - right;
+            }
+        }
+    }
+
+    public interface IOperation
+    {
+        int Operate(int left, int right);
+    }
+
+    public interface IOperationData
+    {
+        Char Symbol { get; }
+    }
+}
---
+++ UnityDemo/Program.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Linq;
+using Microsoft.Practices.ServiceLocation;
+
+namespace UnityDemo
+{
+    internal class Program
+    {
+        public Program(ICalculator[] myCalculator)
+        {
+            myCalculator.FirstOrDefault(p => p.id == 2)?.Calculate();
+        }
+
+        [Dependency]
+        public ICalculator myCalculator { get; set; }
+
+        private static void Main(string[] args)
+        {
+            #region 类型的配置容器注册
+
+            var myContainer = new UnityContainer();
+            myContainer.RegisterType<ICalculator, MyCalculator>(nameof(MyCalculator));
+            myContainer.RegisterType<ICalculator, MyCalculator2>(nameof(MyCalculator2));
+            ServiceLocator.SetLocatorProvider(()=>new UnityServiceLocator(myContainer));
+            var mycalculator= ServiceLocator.Current.GetInstance<ICalculator>(nameof(MyCalculator));
+            var mycalculator2 = myContainer.Resolve<ICalculator>(nameof(MyCalculator2));
+            var mycalculators = ServiceLocator.Current.GetAllInstances<ICalculator>();
+            foreach (var p in mycalculators)
+            {
+                p.Calculate();
+            }
+            var b = mycalculator.Equals(mycalculator2);
+            mycalculator.Calculate();
+            mycalculator2.Calculate();
+
+            #endregion 类型的配置容器注册
+
+            #region 已有对象实例的配置容器注册
+
+            //var myContainer = new UnityContainer();
+            //var myCalculator = new MyCalculator();
+            //myContainer.RegisterInstance<ICalculator>(myCalculator);
+            //var myCalculatorInstence = myContainer.Resolve<ICalculator>();
+            //myCalculatorInstence.Calculate();
+
+            #endregion 已有对象实例的配置容器注册
+
+            #region 构造函数注入
+
+            //var myContainer = new UnityContainer();
+            //myContainer.RegisterType<ICalculator, MyCalculator>(nameof(UnityDemo.MyCalculator));
+            //myContainer.RegisterType<ICalculator, MyCalculator2>(nameof(MyCalculator2));
+
+            //var pro = myContainer.Resolve<Program>();
+            //pro.MyCalculator.Calculate();
+
+            #endregion 构造函数注入
+
+            #region 属性（设置器）注入
+
+            //var myContainer = new UnityContainer();
+            //myContainer.RegisterType<ICalculator, MyCalculator>();
+
+            //var pro = myContainer.Resolve<Program>();
+            //pro.myCalculator.Calculate();
+
+            #endregion 属性（设置器）注入
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface ICalculator
+    {
+        int id { get; set; }
+
+        void Calculate();
+    }
+
+    public class MyCalculator : ICalculator
+    {
+        public int id { get; set; } = 1;
+
+        public void Calculate()
+        {
+            Console.WriteLine("this is MyCalculator");
+        }
+    }
+
+    public class MyCalculator2 : ICalculator
+    {
+        public int id { get; set; } = 2;
+
+        public void Calculate()
+        {
+            Console.WriteLine("this is MyCalculator2");
+        }
+    }
+}
